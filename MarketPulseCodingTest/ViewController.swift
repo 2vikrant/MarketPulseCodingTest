a1ad29673ab7007f16cd58cafd84262169c9484e
ganesh@digicita.com <ganesh@digicita.com>
2019-12-22 11:12:36 +0530

    Initial Commit

---
+++ MarketPulseCodingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarketPulseCodingTest
+//
+//  Created by Ganesh Prasad on 22/12/19.
+//  Copyright © 2019 MarketPulse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
